ebdd9f76e7ed0d50ab08e1442d44768eb13c4e38
swapnil-code <66630121+swapnil-code@users.noreply.github.com>
2022-02-11 17:38:08 +0530

    Add files via upload

---
+++ precache-manifest.ccfb740ac6cf95f4715c7360ed4c801a.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48ed8c026b3c733b2e5d2e5eb0f7c30b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "be41c2c71f9bfa6543eb",
+    "url": "/static/css/2.dedc3531.chunk.css"
+  },
+  {
+    "revision": "f8e6566aefa48237516c",
+    "url": "/static/css/main.c0d2a24b.chunk.css"
+  },
+  {
+    "revision": "be41c2c71f9bfa6543eb",
+    "url": "/static/js/2.3c097907.chunk.js"
+  },
+  {
+    "revision": "f8e6566aefa48237516c",
+    "url": "/static/js/main.4141c3e9.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "55d5567d5080cf9a8f294bad872ffc08",
+    "url": "/static/media/blur-background-1187974.55d5567d.jpg"
+  },
+  {
+    "revision": "1cd48d78f06d33973d9d761d426e69bf",
+    "url": "/static/media/weathericons-regular-webfont.1cd48d78.woff2"
+  },
+  {
+    "revision": "4618f0de2a818e7ad3fe880e0b74d04a",
+    "url": "/static/media/weathericons-regular-webfont.4618f0de.ttf"
+  },
+  {
+    "revision": "4b658767da6bd92ce2addb3ce512784d",
+    "url": "/static/media/weathericons-regular-webfont.4b658767.eot"
+  },
+  {
+    "revision": "8cac70ebda3f23ce472110d9f21e8593",
+    "url": "/static/media/weathericons-regular-webfont.8cac70eb.woff"
+  },
+  {
+    "revision": "ecaf8b481729b18f6a8494d9f691cdae",
+    "url": "/static/media/weathericons-regular-webfont.ecaf8b48.svg"
+  }
+]);
